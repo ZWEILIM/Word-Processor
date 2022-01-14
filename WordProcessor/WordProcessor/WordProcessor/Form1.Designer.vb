1f489d1b40e87119e75cbc6c129d7303fc6361e2
Zhong Wei <61281080+ZWEILIM@users.noreply.github.com>
2022-01-14 18:46:08 +0800

    First Release

---
+++ WordProcessor/WordProcessor/WordProcessor/Form1.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.msMain = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SelectAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsMain = New System.Windows.Forms.ToolStrip()
+        Me.tsbNew = New System.Windows.Forms.ToolStripButton()
+        Me.tsbOpen = New System.Windows.Forms.ToolStripButton()
+        Me.tsbSave = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbBold = New System.Windows.Forms.ToolStripButton()
+        Me.tsbItalic = New System.Windows.Forms.ToolStripButton()
+        Me.tsbUnderline = New System.Windows.Forms.ToolStripButton()
+        Me.tsbStrikeCut = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tscSize = New System.Windows.Forms.ToolStripComboBox()
+        Me.OpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFile = New System.Windows.Forms.SaveFileDialog()
+        Me.rtbContent = New System.Windows.Forms.RichTextBox()
+        Me.msMain.SuspendLayout()
+        Me.tsMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'msMain
+        '
+        Me.msMain.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.msMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.InsertToolStripMenuItem})
+        Me.msMain.Location = New System.Drawing.Point(0, 0)
+        Me.msMain.Name = "msMain"
+        Me.msMain.Size = New System.Drawing.Size(465, 28)
+        Me.msMain.TabIndex = 0
+        Me.msMain.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ToolStripMenuItem1, Me.QuitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(242, 26)
+        Me.NewToolStripMenuItem.Text = "&New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(242, 26)
+        Me.OpenToolStripMenuItem.Text = "&Open..."
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(242, 26)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(242, 26)
+        Me.SaveAsToolStripMenuItem.Text = "Save &As..."
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(239, 6)
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(242, 26)
+        Me.QuitToolStripMenuItem.Text = "&Quit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem, Me.ToolStripMenuItem2, Me.SelectAllToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(206, 26)
+        Me.CutToolStripMenuItem.Text = "C&ut"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(206, 26)
+        Me.CopyToolStripMenuItem.Text = "&Copy"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(206, 26)
+        Me.PasteToolStripMenuItem.Text = "&Paste"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(203, 6)
+        '
+        'SelectAllToolStripMenuItem
+        '
+        Me.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem"
+        Me.SelectAllToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(206, 26)
+        Me.SelectAllToolStripMenuItem.Text = "&Select All"
+        '
+        'InsertToolStripMenuItem
+        '
+        Me.InsertToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PictureToolStripMenuItem})
+        Me.InsertToolStripMenuItem.Name = "InsertToolStripMenuItem"
+        Me.InsertToolStripMenuItem.Size = New System.Drawing.Size(59, 24)
+        Me.InsertToolStripMenuItem.Text = "&Insert"
+        '
+        'PictureToolStripMenuItem
+        '
+        Me.PictureToolStripMenuItem.Name = "PictureToolStripMenuItem"
+        Me.PictureToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.P), System.Windows.Forms.Keys)
+        Me.PictureToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.PictureToolStripMenuItem.Text = "&Picture..."
+        '
+        'tsMain
+        '
+        Me.tsMain.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.tsMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbNew, Me.tsbOpen, Me.tsbSave, Me.ToolStripSeparator1, Me.tsbBold, Me.tsbItalic, Me.tsbUnderline, Me.tsbStrikeCut, Me.ToolStripSeparator2, Me.tscSize})
+        Me.tsMain.Location = New System.Drawing.Point(0, 28)
+        Me.tsMain.Name = "tsMain"
+        Me.tsMain.Size = New System.Drawing.Size(465, 28)
+        Me.tsMain.TabIndex = 1
+        Me.tsMain.Text = "ToolStrip1"
+        '
+        'tsbNew
+        '
+        Me.tsbNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbNew.Image = CType(resources.GetObject("tsbNew.Image"), System.Drawing.Image)
+        Me.tsbNew.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbNew.Name = "tsbNew"
+        Me.tsbNew.Size = New System.Drawing.Size(29, 25)
+        Me.tsbNew.Text = "New"
+        '
+        'tsbOpen
+        '
+        Me.tsbOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbOpen.Image = CType(resources.GetObject("tsbOpen.Image"), System.Drawing.Image)
+        Me.tsbOpen.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbOpen.Name = "tsbOpen"
+        Me.tsbOpen.Size = New System.Drawing.Size(29, 25)
+        Me.tsbOpen.Text = "Open"
+        '
+        'tsbSave
+        '
+        Me.tsbSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbSave.Image = CType(resources.GetObject("tsbSave.Image"), System.Drawing.Image)
+        Me.tsbSave.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbSave.Name = "tsbSave"
+        Me.tsbSave.Size = New System.Drawing.Size(29, 25)
+        Me.tsbSave.Text = "Save"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 28)
+        '
+        'tsbBold
+        '
+        Me.tsbBold.CheckOnClick = True
+        Me.tsbBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbBold.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tsbBold.Image = CType(resources.GetObject("tsbBold.Image"), System.Drawing.Image)
+        Me.tsbBold.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbBold.Name = "tsbBold"
+        Me.tsbBold.Size = New System.Drawing.Size(29, 25)
+        Me.tsbBold.Text = "B"
+        '
+        'tsbItalic
+        '
+        Me.tsbItalic.CheckOnClick = True
+        Me.tsbItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbItalic.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tsbItalic.Image = CType(resources.GetObject("tsbItalic.Image"), System.Drawing.Image)
+        Me.tsbItalic.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbItalic.Name = "tsbItalic"
+        Me.tsbItalic.Size = New System.Drawing.Size(29, 25)
+        Me.tsbItalic.Text = "I"
+        '
+        'tsbUnderline
+        '
+        Me.tsbUnderline.CheckOnClick = True
+        Me.tsbUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbUnderline.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tsbUnderline.Image = CType(resources.GetObject("tsbUnderline.Image"), System.Drawing.Image)
+        Me.tsbUnderline.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbUnderline.Name = "tsbUnderline"
+        Me.tsbUnderline.Size = New System.Drawing.Size(29, 25)
+        Me.tsbUnderline.Text = "U"
+        '
+        'tsbStrikeCut
+        '
+        Me.tsbStrikeCut.CheckOnClick = True
+        Me.tsbStrikeCut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbStrikeCut.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tsbStrikeCut.Image = CType(resources.GetObject("tsbStrikeCut.Image"), System.Drawing.Image)
+        Me.tsbStrikeCut.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbStrikeCut.Name = "tsbStrikeCut"
+        Me.tsbStrikeCut.Size = New System.Drawing.Size(29, 25)
+        Me.tsbStrikeCut.Text = "S"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 28)
+        '
+        'tscSize
+        '
+        Me.tscSize.Items.AddRange(New Object() {"2", "4", "7.8", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36"})
+        Me.tscSize.Name = "tscSize"
+        Me.tscSize.Size = New System.Drawing.Size(75, 28)
+        '
+        'OpenFile
+        '
+        Me.OpenFile.Title = "Select a file to be open"
+        '
+        'SaveFile
+        '
+        Me.SaveFile.Filter = "RTF document|*.rtf"
+        Me.SaveFile.Title = "Type your file name..."
+        '
+        'rtbContent
+        '
+        Me.rtbContent.Location = New System.Drawing.Point(0, 58)
+        Me.rtbContent.Name = "rtbContent"
+        Me.rtbContent.Size = New System.Drawing.Size(422, 343)
+        Me.rtbContent.TabIndex = 2
+        Me.rtbContent.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 441)
+        Me.Controls.Add(Me.rtbContent)
+        Me.Controls.Add(Me.tsMain)
+        Me.Controls.Add(Me.msMain)
+        Me.MainMenuStrip = Me.msMain
+        Me.Name = "Form1"
+        Me.Text = "Word Processor"
+        Me.msMain.ResumeLayout(False)
+        Me.msMain.PerformLayout()
+        Me.tsMain.ResumeLayout(False)
+        Me.tsMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents msMain As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents SelectAllToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tsMain As ToolStrip
+    Friend WithEvents OpenFile As OpenFileDialog
+    Friend WithEvents SaveFile As SaveFileDialog
+    Friend WithEvents InsertToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tsbNew As ToolStripButton
+    Friend WithEvents tsbOpen As ToolStripButton
+    Friend WithEvents tsbSave As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents tsbBold As ToolStripButton
+    Friend WithEvents tsbItalic As ToolStripButton
+    Friend WithEvents tsbUnderline As ToolStripButton
+    Friend WithEvents tsbStrikeCut As ToolStripButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents tscSize As ToolStripComboBox
+    Friend WithEvents rtbContent As RichTextBox
+End Class
---
+++ WordProcessor/WordProcessor/WordProcessor/Form1.vb
@@ -0,0 +1,280 @@
+﻿Public Class Form1
+
+    'store file
+    Dim filename As String = ""
+
+    'mark current document is dirty 
+    Dim dirty As Boolean = False
+
+    Private Sub rtbContent_Resize(sender As Object, e As EventArgs) Handles rtbContent.Resize
+
+        'resize textbox 
+        rtbContent.Size = New Size(Me.ClientSize.Width, Me.ClientSize.Height - tsMain.Height - msMain.Height)
+
+
+    End Sub
+
+    Private Sub rtbContent_TextChanged(sender As Object, e As EventArgs) Handles rtbContent.TextChanged, rtbContent.StyleChanged, rtbContent.SizeChanged
+
+        'set docement as dirty 
+        dirty = True
+
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+
+        rtbContent.Cut()
+
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        rtbContent.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        rtbContent.Paste()
+    End Sub
+
+    Private Sub SelectAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem.Click
+        rtbContent.SelectAll()
+    End Sub
+
+    Sub style(style As FontStyle)
+
+        'store current font style
+        Dim current As FontStyle = rtbContent.SelectionFont.Style
+
+        If style = FontStyle.Bold Then
+            'check if bold is off
+            If rtbContent.SelectionFont.Bold = False Then
+
+                'add bold
+                current += FontStyle.Bold
+
+            Else
+
+                'take away bold
+                current -= FontStyle.Bold
+                rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, FontStyle.Regular)
+
+            End If
+
+            'replace current font with new font style
+            rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, current)
+
+        ElseIf style = FontStyle.Italic Then
+
+            'check if Italic is off
+            If rtbContent.SelectionFont.Italic = False Then
+
+                'add Italic
+                current += FontStyle.Italic
+
+            Else
+
+                'take away Italic
+                current -= FontStyle.Italic
+                rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, FontStyle.Regular)
+
+            End If
+
+            'replace current font with new font style
+            rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, current)
+
+        ElseIf style = FontStyle.Underline Then
+
+            'check if Underline is off
+            If rtbContent.SelectionFont.Underline = False Then
+
+                'add Underline
+                current += FontStyle.Underline
+
+            Else
+
+                'take away Underline
+                current -= FontStyle.Underline
+                rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, FontStyle.Regular)
+
+            End If
+
+            'replace current font with new font style
+            rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, current)
+
+        ElseIf style = FontStyle.Strikeout Then
+
+            'check if Strikeout is off
+            If rtbContent.SelectionFont.Strikeout = False Then
+
+                'add Strikeout
+                current += FontStyle.Strikeout
+
+            Else
+
+                'take away Strikeout
+                current -= FontStyle.Strikeout
+                rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, FontStyle.Regular)
+
+            End If
+
+            'replace current font with new font style
+            rtbContent.SelectionFont = New Font(rtbContent.SelectionFont, current)
+
+        End If
+
+    End Sub
+
+
+    Private Sub tsbBold_Click(sender As Object, e As EventArgs) Handles tsbBold.Click
+        style(FontStyle.Bold)
+    End Sub
+
+    Private Sub tsbItalic_Click(sender As Object, e As EventArgs) Handles tsbItalic.Click
+        style(FontStyle.Italic)
+    End Sub
+
+    Private Sub tsbUnderline_Click(sender As Object, e As EventArgs) Handles tsbUnderline.Click
+        style(FontStyle.Underline)
+    End Sub
+
+    Private Sub tsbStrikeCut_Click(sender As Object, e As EventArgs) Handles tsbStrikeCut.Click
+        style(FontStyle.Strikeout)
+    End Sub
+
+    Private Sub tscSize_TextChanged(sender As Object, e As EventArgs) Handles tscSize.TextChanged
+
+        'take number and convert into single
+        Dim newsize As Single = Convert.ToSingle(tscSize.Text)
+
+        'Apply new size
+        rtbContent.SelectionFont = New Font(rtbContent.SelectionFont.FontFamily, newsize)
+
+
+
+    End Sub
+
+    Private Sub PictureToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PictureToolStripMenuItem.Click
+
+        'set file filter
+        OpenFile.Filter = "Image Files|*.jpg;*.png;*.gif;*.tiff"
+
+        'if user press OK
+        If OpenFile.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            'load picture
+            Dim img As Image = Image.FromFile(OpenFile.FileName)
+
+            'copy into clipboard and save to textbox
+            Clipboard.SetImage(img)
+            rtbContent.Paste()
+        End If
+
+
+
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+
+        'open save file dialog
+        If SaveFile.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            'save file
+            rtbContent.SaveFile(SaveFile.FileName)
+            dirty = False
+
+            'remember the file name 
+            filename = SaveFile.FileName
+
+            'set form title to the filename
+            Me.Text = IO.Path.GetFileName(filename)
+
+        End If
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+        'check if file have been save
+        If filename = "" Then
+
+            'call on the save as button
+            SaveAsToolStripMenuItem_Click(Nothing, Nothing)
+
+        Else
+
+            'save file
+            rtbContent.SaveFile(SaveFile.FileName)
+            dirty = False
+
+        End If
+
+    End Sub
+    Sub checkdirty()
+
+        'check if the file have been save or not
+        If dirty = True Then
+
+            If MessageBox.Show("Do you want to save ?", "Confirm?", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+
+                SaveAsToolStripMenuItem_Click(Nothing, Nothing)
+
+            End If
+
+        End If
+
+    End Sub
+    Private Sub QuitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitToolStripMenuItem.Click
+
+        checkdirty()
+
+        'close form
+        Me.Close()
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        checkdirty()
+
+        'setup new text box 
+        filename = ""
+        dirty = False
+        Me.Text = "Word Processor"
+        rtbContent.ResetText()
+
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+
+        dirty = False
+        checkdirty()
+
+        OpenFile.Filter = "RTF document|*.rtf"
+
+        If OpenFile.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            rtbContent.LoadFile(OpenFile.FileName)
+            dirty = False
+            filename = OpenFile.FileName
+            Me.Text = IO.Path.GetFileName(filename)
+
+        End If
+
+    End Sub
+
+    Private Sub tsbNew_Click(sender As Object, e As EventArgs) Handles tsbNew.Click
+
+        NewToolStripMenuItem_Click(Nothing, Nothing)
+
+    End Sub
+
+    Private Sub tsbOpen_Click(sender As Object, e As EventArgs) Handles tsbOpen.Click
+
+        OpenToolStripMenuItem_Click(Nothing, Nothing)
+
+    End Sub
+
+    Private Sub tsbSave_Click(sender As Object, e As EventArgs) Handles tsbSave.Click
+
+        SaveToolStripMenuItem_Click(Nothing, Nothing)
+
+    End Sub
+End Class
